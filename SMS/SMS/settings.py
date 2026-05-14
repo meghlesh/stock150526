f683377a4ea36be68533540278fa517b3d97fdef
meghlesh <meghachaudhari24@gmail.com>
2026-05-14 20:49:07 +0530

    Update settings.py

--- SMS/SMS/settings.py
+++ SMS/SMS/settings.py
@@ -144,7 +144,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 SITE_URL = 'http://127.0.0.1:8000'  #Change to your domain in production
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
